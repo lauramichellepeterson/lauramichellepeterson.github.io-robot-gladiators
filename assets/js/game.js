2160eddc9f87aa408fa22af6855cab4c4a859ed5
Laura Micheller Peterson <michellepeterson.mishmish@gmail.com>
2020-11-09 18:10:01 -0700

    added html basics and started java user interface

---
+++ assets/js/game.js
@@ -0,0 +1,23 @@
+// Ask user what they want to name their robot 
+window.prompt("What is your robot's name?");
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+var name = "your name";
+console.log(name);
+
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+ // fight();
